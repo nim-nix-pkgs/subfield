fcb16ba64ae7add44be2702e8f589c173db5c492
hugosenari <hugosenari@gmail.com>
2022-03-12 15:28:57 -0300

    chore: re index subfield

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Override the dot operator to access nested subfields of a Nim object.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-subfield-master.flake = false;
+  inputs.src-subfield-master.owner = "jyapayne";
+  inputs.src-subfield-master.ref   = "refs/heads/master";
+  inputs.src-subfield-master.repo  = "subfield";
+  inputs.src-subfield-master.type  = "github";
+  
+  inputs."einheit".dir   = "nimpkgs/e/einheit";
+  inputs."einheit".owner = "riinr";
+  inputs."einheit".ref   = "flake-pinning";
+  inputs."einheit".repo  = "flake-nimble";
+  inputs."einheit".type  = "github";
+  inputs."einheit".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."einheit".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-subfield-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-subfield-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
